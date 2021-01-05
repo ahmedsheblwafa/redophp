1a9d729d2ef781232532ed993c259696fe0fb912
AlaaSaadallah <alaa.saadallah96@gmail.com>
2021-01-05 19:18:49 +0200

    edit database name

--- connection.php
+++ connection.php
@@ -5,7 +5,7 @@ require_once('checkCookies.php');
 define("DB_SERVER", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");
-define("DB_NAME", "caferia-project");
+define("DB_NAME", "cafetria");
 define("DB_PORT","3306");
 
 $conn=mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME,DB_PORT);
